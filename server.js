997a7d23fcc92dcb83109f203e3ada9472dc7085
Rich <sarvesh123305@gmail.com>
2022-01-24 17:15:24 +0530

    Express server setup with initial route

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+//const res = require("express/lib/response");
+const app = express();
+app.get("/", (req, res) => res.json({ msg: "Hello json World!" }));
+app.use("/api/users", require("./routes/users"));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/contacts", require("./routes/contacts"));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server stared on port ${PORT}`));
